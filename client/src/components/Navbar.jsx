030b9139c68c7a75a59c350501486f9822cc267d
= <=>
2023-03-16 17:42:34 +0530

    first of skpt

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,15 @@
+import {HiMenuAlt4} from 'react-icons/hi';
+import {AiOutlineClose} from 'react-icons/ai';
+
+//logo
+import logo from  '../assets/logo/SkPTlogo.png';
+
 const Navbar = ()=>{
     return(
         <div>
+            <div>
+                <img src={logo} alt="logo" height={80} width={80} />
+            </div>
             <h3>Navbar</h3>
         </div>
     )
